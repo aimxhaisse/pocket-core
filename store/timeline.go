89761f5ed45ad51f4356d1d97031eae9effb8a9e
mxs <mxs@sbrk.org>
2021-05-24 11:51:58 +0200

    feat: add block timestamp in the sandwalker db

--- store/timeline.go
+++ store/timeline.go
@@ -1,28 +1,22 @@
 package store
 
 import (
-	"log"
 	"database/sql"
+	"log"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var timelineDb *sql.DB = nil
 
-type TxType = int64
-const (
-	TxMint TxType = iota
-	TxSlash
-	TxSimpleSlash
-)
-
-func InsertTimelineEvent(account string, height int64, amount int64, txtype TxType) error {
-	statement, err := timelineDb.Prepare("INSERT INTO timeline (account, block, amount, txtype) VALUES (?, ?, ?, ?)")
+func InsertTimelineEvent(account string, block int64, when time.Time, amount int64) error {
+	statement, err := timelineDb.Prepare("INSERT INTO timeline (account, block, time, amount) VALUES (?, ?, ?, ?, ?)")
 	if err != nil {
 		log.Printf("unable to prepare timeline insert query: %v", err)
 		return err
 	}
-	_, err = statement.Exec(account, height, amount, txtype)
+	_, err = statement.Exec(account, block, when, amount)
 	if err != nil {
 		log.Printf("unable to exec timeline insert query: %v", err)
 		return err
@@ -30,14 +24,14 @@ func InsertTimelineEvent(account string, height int64, amount int64, txtype TxTy
 	return nil
 }
 
-func init () {
+func init() {
 	var err error
 
 	timelineDb, err = sql.Open("sqlite3", "timeline.db")
 	if err != nil {
 		log.Fatalf("unable to open timeline db: %v", err)
 	}
-	statement, err := timelineDb.Prepare("CREATE TABLE IF NOT EXISTS timeline(account TEXT, block INTEGER, amount INTEGER, txtype INTEGER)")
+	statement, err := timelineDb.Prepare("CREATE TABLE IF NOT EXISTS timeline(account TEXT, block INTEGER, time DATETIME, amount INTEGER)")
 	if err != nil {
 		log.Fatalf("unable to prepare create table statement in timeline db: %v", err)
 	}
--- x/nodes/keeper/slash.go
+++ x/nodes/keeper/slash.go
@@ -2,7 +2,6 @@ package keeper
 
 import (
 	"fmt"
-	"github.com/pokt-network/pocket-core/store"
 	"time"
 
 	"github.com/pokt-network/pocket-core/x/nodes/exported"
@@ -48,7 +47,6 @@ func (k Keeper) simpleSlash(ctx sdk.Ctx, addr sdk.Address, amount sdk.BigInt) {
 		}
 	}
 
-	store.InsertTimelineEvent(validator.GetAddress().String(), ctx.BlockHeight(), -amount.Int64(), store.TxSimpleSlash)
 	// Log that a slash occurred
 	ctx.Logger().Info(fmt.Sprintf("validator %s simple slashed; burned %s tokens",
 		validator.GetAddress(), amount.String()))
@@ -112,7 +110,6 @@ func (k Keeper) slash(ctx sdk.Ctx, addr sdk.Address, infractionHeight, power int
 		}
 	}
 
-	store.InsertTimelineEvent(validator.GetAddress().String(), ctx.BlockHeight(), -tokensToBurn.Int64(), store.TxSlash)
 	// Log that a slash occurred
 	logger.Info(fmt.Sprintf("validator %s slashed by slash factor of %s; burned %v tokens",
 		validator.GetAddress(), slashFactor.String(), tokensToBurn))
