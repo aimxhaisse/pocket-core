30bbacc83765c86ac402b3d359a085004ac074a5
mxs <mxs@sbrk.org>
2021-05-25 00:29:11 +0200

    feat: add primary key to timeline

--- store/timeline.go
+++ store/timeline.go
@@ -11,7 +11,7 @@ import (
 var timelineDb *sql.DB = nil
 
 func InsertTimelineEvent(account string, block int64, when time.Time, amount int64) error {
-	statement, err := timelineDb.Prepare("INSERT INTO timeline (account, block, time, amount) VALUES (?, ?, ?, ?)")
+	statement, err := timelineDb.Prepare("INSERT INTO timeline (id, account, block, time, amount) VALUES (null, ?, ?, ?, ?)")
 	if err != nil {
 		log.Printf("unable to prepare timeline insert query: %v", err)
 		return err
@@ -31,7 +31,7 @@ func init() {
 	if err != nil {
 		log.Fatalf("unable to open timeline db: %v", err)
 	}
-	statement, err := timelineDb.Prepare("CREATE TABLE IF NOT EXISTS timeline(account TEXT, block INTEGER, time DATETIME, amount INTEGER)")
+	statement, err := timelineDb.Prepare("CREATE TABLE IF NOT EXISTS timeline(id INTEGER NOT NULL PRIMARY KEY, account TEXT, block INTEGER, time DATETIME, amount INTEGER)")
 	if err != nil {
 		log.Fatalf("unable to prepare create table statement in timeline db: %v", err)
 	}
