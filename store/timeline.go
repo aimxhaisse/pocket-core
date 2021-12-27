a7325f8530b83f7015f4f198b2a8962011f85a8b
mxs <mxs@sbrk.org>
2021-12-27 15:43:37 +0000

    Revert "update: Added slashes & simple slashes to timeline db"
    This reverts commit e23fe73aaeacdbea405d9248223164aa504b3dc0.

--- store/timeline.go
+++ store/timeline.go
@@ -9,20 +9,13 @@ import (
 
 var timelineDb *sql.DB = nil
 
-type TxType = int64
-const (
-	TxMint TxType = iota
-	TxSlash
-	TxSimpleSlash
-)
-
-func InsertTimelineEvent(account string, height int64, amount int64, txtype TxType) error {
-	statement, err := timelineDb.Prepare("INSERT INTO timeline (account, block, amount, txtype) VALUES (?, ?, ?, ?)")
+func InsertTimelineEvent(account string, height int64, amount int64) error {
+	statement, err := timelineDb.Prepare("INSERT INTO timeline (account, block, amount) VALUES (?, ?, ?)")
 	if err != nil {
 		log.Printf("unable to prepare timeline insert query: %v", err)
 		return err
 	}
-	_, err = statement.Exec(account, height, amount, txtype)
+	_, err = statement.Exec(account, height, amount)
 	if err != nil {
 		log.Printf("unable to exec timeline insert query: %v", err)
 		return err
@@ -37,7 +30,7 @@ func init () {
 	if err != nil {
 		log.Fatalf("unable to open timeline db: %v", err)
 	}
-	statement, err := timelineDb.Prepare("CREATE TABLE IF NOT EXISTS timeline(account TEXT, block INTEGER, amount INTEGER, txtype INTEGER)")
+	statement, err := timelineDb.Prepare("CREATE TABLE IF NOT EXISTS timeline(account TEXT, block INTEGER, amount INTEGER)")
 	if err != nil {
 		log.Fatalf("unable to prepare create table statement in timeline db: %v", err)
 	}
